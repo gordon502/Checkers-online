6dea35b41a85daf95f7ff839c527597fc55f6e18
gordon502 <gordonarek@gmail.com>
2019-12-02 17:46:45 +0100

    Repair bad merge

--- server/main.c
+++ server/main.c
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-<<<<<<< HEAD
 #include "singleGameLogic.h"
-=======
-#include "singleGamelogic.h"
->>>>>>> 97eb0b329478232915fe06d6d62c2aa0082fe7c4
+
 
 int main(int argc, char* argv[]){
     startNewGame();
